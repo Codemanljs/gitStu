a4021349a39f22194425fe7381986637a0196b09
ljs <904617189@qq.com>
2023-03-08 16:50:14 +0800

    create helloController

---
+++ src/main/java/com/example/springbootgit/config/MyConfig.java
@@ -0,0 +1,5 @@
+package com.example.springbootgit.config;/**
+@author ljs
+@create 2023-03-08 16:47
+*/public class MyConfig {
+}
---
+++ src/main/java/com/example/springbootgit/controller/HelloController.java
@@ -0,0 +1,5 @@
+package com.example.springbootgit.controller;/**
+@author ljs
+@create 2023-03-08 16:48
+*/public class HelloController {
+}
